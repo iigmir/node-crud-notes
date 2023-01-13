5565276492b2cf9c02a2dbfadb8258b066d6d1f8
iigmir <roc120j@gmail.com>
2023-01-14 00:29:08 +0800

    FormData make everyone suffer

--- static/index.js
+++ static/index.js
@@ -13,12 +13,13 @@ const user_form = document.querySelector("form#user-form");
 user_form.addEventListener("submit", (event) => {
     event.preventDefault();
     const form_dom = event.target;
+    const form_body = new URLSearchParams(new FormData(form_dom));
     const ajax = fetch("/api/users", {
         method: "POST",
         headers: {
             "Content-Type": `application/x-www-form-urlencoded; charset=UTF-8`,
         },
-        body: new FormData(form_dom)
+        body: form_body.toString()
     }).then( json_format );
     ajax.then( res => {
         console.log(res);
